befdd0b9ecabfe8513836dda3e71a5e2bcae5456
KevinPXu <kevinpxu21@gmail.com>
2022-10-02 09:30:16 -0500

    created new render question function to dynamically create the elements of the answer choices

--- script.js
+++ script.js
@@ -6,59 +6,67 @@ var choice1El = document.querySelector("#choice1");
 var choice2El = document.querySelector("#choice2");
 var choice3El = document.querySelector("#choice3");
 var choice4El = document.querySelector("#choice4");
-var questionNum = 0;
+var questionCount = 0;
 
 var questionBank = [
   {
     question: "What does the method split do to a string?",
-    answerChoices: {
-      a: "splits the last character off the string",
-      b: "deconstructs the string and places it into an array",
-      c: "splits the first character off the string",
-      d: "splits the string into two user selected variables",
-    },
-    correctAnswer: "b",
+    answerChoices: [
+      "splits the last character off the string",
+      "deconstructs the string and places it into an array",
+      "splits the first character off the string",
+      "splits the string into two user selected variables",
+    ],
+    correctAnswer: "2",
   },
   {
     question:
       "Where is the correct place to insert a JavaScript in an HTML document?",
-    answerChoices: {
-      a: "Both the <head> section and the <body> section are correct",
-      b: "The bottom of the <body> section",
-      c: "The <head> section",
-      d: "The footer section",
-    },
-    correctAnswer: "b",
+    answerChoices: [
+      "Both the <head> section and the <body> section are correct",
+      "The bottom of the <body> section",
+      "The <head> section",
+      "The footer section",
+    ],
+    correctAnswer: "2",
   },
   {
     question: 'How do you call a function named "myFunction"?',
-    answerChoices: {
-      a: "call myFunction()",
-      b: "call function myFunction",
-      c: "Call.myFunction()",
-      d: "myFunction()",
-    },
-    correctAnswer: "d",
+    answerChoices: [
+      "call myFunction()",
+      "call function myFunction",
+      "Call.myFunction()",
+      "myFunction()",
+    ],
+    correctAnswer: "2",
   },
 ];
 
 startQuiz();
 //initially displays the first question when program starts
 function startQuiz() {
+  var test = 1;
   console.log("hello");
-  renderQuestions(questionNum);
+  renderQuestions(questionCount);
+}
+
+While (questionCount < questionBank.length) {
+    
 }
 
 function renderQuestions(index) {
   console.log("hello");
+  questionEl.textContent = questionBank[index].question;
   answerChoiceEl.innerHTML = " ";
-  for (var i = 0; i < 4; i++) {
+  for (var i = 1; i < questionBank[index].answerChoices.length + 1; i++) {
     var ansChoice = document.createElement("button");
     console.log(ansChoice);
-    ansChoice.setAttribute("data-choice", "[i]");
-    ansChoice.textContent(questionBank[index].answerChoices[i]);
+    ansChoice.setAttribute("data-choice", [i]);
+    var ansChoiceCont = document.createTextNode(
+      questionBank[index].answerChoices[i]
+    );
+    ansChoice.appendChild(ansChoiceCont);
     answerChoiceEl.appendChild(ansChoice);
-    console.log(ansChoice);
   }
 }
 
